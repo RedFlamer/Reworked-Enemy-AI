0ab8a947df05fabe8c7bb062e458da04b1e37bf6
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2021-07-17 23:16:25 +0100

    fixed civilians not following beyond the initial position

--- Reworked Enemy AI/copactionwalk.lua
+++ Reworked Enemy AI/copactionwalk.lua
@@ -1,6 +1,41 @@
 local react_shoot = AIAttentionObject.REACT_SHOOT
 local react_surprised = AIAttentionObject.REACT_SURPRISED
 
+--[[function CopActionWalk._calculate_simplified_path(good_pos, original_path, nr_iterations, is_host, apply_padding)
+	local simplified_path = {good_pos}
+	local original_path_size = #original_path
+
+	for i = 1, #original_path do
+		local nav_point = original_path[i]
+	end
+
+	for i_nav_point, nav_point in ipairs(original_path) do
+		if nav_point.x and i_nav_point ~= original_path_size and (i_nav_point == 1 or simplified_path[#simplified_path].x) then
+			local pos_from = simplified_path[#simplified_path]
+			local pos_to = CopActionWalk._nav_point_pos(original_path[i_nav_point + 1])
+			local add_point = is_host and math.abs(nav_point.z - pos_from.z - (nav_point.z - pos_to.z)) > 60
+			add_point = add_point or CopActionWalk._chk_shortcut_pos_to_pos(pos_from, pos_to)
+
+			if add_point then
+				simplified_path[#simplified_path + 1] = mvec3_cpy(nav_point)
+			end
+		else
+			simplified_path[#simplified_path + 1] = nav_point
+		end
+	end
+
+	if apply_padding and #simplified_path > 2 then
+		CopActionWalk._apply_padding_to_simplified_path(simplified_path)
+		CopActionWalk._calculate_shortened_path(simplified_path)
+	end
+
+	if nr_iterations > 1 and #simplified_path > 2 then
+		simplified_path = CopActionWalk._calculate_simplified_path(good_pos, simplified_path, nr_iterations - 1, is_host, apply_padding)
+	end
+
+	return simplified_path
+end--]]
+
 function CopActionWalk:on_attention(attention)
 	if attention then
 		self._attention = attention
--- Reworked Enemy AI/coplogicidle.lua
+++ Reworked Enemy AI/coplogicidle.lua
@@ -1,6 +1,8 @@
 local math_min = math.min
 
+local mvec3_cpy = mvector3.copy
 local mvec3_dis = mvector3.distance
+local mvec3_dis_sq = mvector3.distance_sq
 local mvec3_equal = mvector3.equal
 
 local react_arrest = AIAttentionObject.REACT_ARREST
@@ -65,49 +67,58 @@ function CopLogicIdle._chk_reaction_to_attention_object(data, attention_data, st
 	return math_min(reaction, react_combat)
 end
 
-function CopLogicIdle._chk_relocate( data )
+function CopLogicIdle._chk_relocate(data)
 	if data.objective and data.objective.type == "follow" then
 		if data.is_converted then
-			if TeamAILogicIdle._check_should_relocate( data, data.internal_data, data.objective ) then
+			if TeamAILogicIdle._check_should_relocate(data, data.internal_data, data.objective) then
 				data.objective.in_place = nil
-				CopLogicIdle._exit( data.unit, "travel" )
+
+				data.logic._exit(data.unit, "travel")
+
 				return true
 			end
+
 			return
 		end
-		
-		local relocate
+
+		if data.is_tied and data.objective.lose_track_dis and data.objective.lose_track_dis * data.objective.lose_track_dis < mvector3.distance_sq(data.m_pos, data.objective.follow_unit:movement():m_pos()) then
+			data.brain:set_objective(nil)
+
+			return true
+		end
+
+		local relocate = nil
 		local follow_unit = data.objective.follow_unit
-		
 		local advance_pos = follow_unit:brain() and follow_unit:brain():is_advancing()
-		local follow_unit_pos = advance_pos or data.m_pos
-		
-		if data.objective.relocated_to and mvec3_equal( data.objective.relocated_to, follow_unit_pos ) then
-			-- we have already relocated to this pos
+		local follow_unit_pos = advance_pos or follow_unit:movement():m_pos()
+
+		if data.objective.relocated_to and mvector3.equal(data.objective.relocated_to, follow_unit_pos) then
 			return
 		end
-		
-		if data.objective.distance and data.objective.distance < mvec3_dis(data.m_pos, follow_unit_pos) then
-			--print( "relocating due to distance" )
+
+		if data.objective.distance and data.objective.distance < mvector3.distance(data.m_pos, follow_unit_pos) then
 			relocate = true
 		end
-		
+
 		if not relocate then
 			local ray_params = {
 				tracker_from = data.unit:movement():nav_tracker(),
 				pos_to = follow_unit_pos
 			}
-			local ray_res = managers.navigation:raycast( ray_params )
+			local ray_res = managers.navigation:raycast(ray_params)
+
 			if ray_res then
-				--print( "relocating due to obstacle" )
 				relocate = true
 			end
 		end
+
 		if relocate then
 			data.objective.in_place = nil
 			data.objective.nav_seg = follow_unit:movement():nav_tracker():nav_segment()
-			data.objective.relocated_to = mvector3.copy( follow_unit_pos )
-			CopLogicBase._exit( data.unit, "travel" )
+			data.objective.relocated_to = mvector3.copy(follow_unit_pos)
+
+			data.logic._exit(data.unit, "travel")
+
 			return true
 		end
 	end
