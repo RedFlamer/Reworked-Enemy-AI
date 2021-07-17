2616528a7b0aa4f0a2a18c1c04bfd2c276d3f886
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2021-07-17 23:17:41 +0100

    Add files via upload

--- Reworked Enemy AI/coplogicidle.lua
+++ Reworked Enemy AI/coplogicidle.lua
@@ -81,7 +81,7 @@ function CopLogicIdle._chk_relocate(data)
 			return
 		end
 
-		if data.is_tied and data.objective.lose_track_dis and data.objective.lose_track_dis * data.objective.lose_track_dis < mvector3.distance_sq(data.m_pos, data.objective.follow_unit:movement():m_pos()) then
+		if data.is_tied and data.objective.lose_track_dis and data.objective.lose_track_dis * data.objective.lose_track_dis < mvec3_dis_sq(data.m_pos, data.objective.follow_unit:movement():m_pos()) then
 			data.brain:set_objective(nil)
 
 			return true
@@ -92,11 +92,11 @@ function CopLogicIdle._chk_relocate(data)
 		local advance_pos = follow_unit:brain() and follow_unit:brain():is_advancing()
 		local follow_unit_pos = advance_pos or follow_unit:movement():m_pos()
 
-		if data.objective.relocated_to and mvector3.equal(data.objective.relocated_to, follow_unit_pos) then
+		if data.objective.relocated_to and mvec3_equal(data.objective.relocated_to, follow_unit_pos) then
 			return
 		end
 
-		if data.objective.distance and data.objective.distance < mvector3.distance(data.m_pos, follow_unit_pos) then
+		if data.objective.distance and data.objective.distance < mvec3_dis(data.m_pos, follow_unit_pos) then
 			relocate = true
 		end
 
@@ -115,7 +115,7 @@ function CopLogicIdle._chk_relocate(data)
 		if relocate then
 			data.objective.in_place = nil
 			data.objective.nav_seg = follow_unit:movement():nav_tracker():nav_segment()
-			data.objective.relocated_to = mvector3.copy(follow_unit_pos)
+			data.objective.relocated_to = mvec3_cpy(follow_unit_pos)
 
 			data.logic._exit(data.unit, "travel")
 
