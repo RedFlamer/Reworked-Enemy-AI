892b4699b0d766088f2649c1fd14044a20b90303
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2021-07-18 20:10:07 +0100

    cleanup but on the right branch

--- Reworked Enemy AI/copactionwalk.lua
+++ Reworked Enemy AI/copactionwalk.lua
@@ -1,31 +1,7 @@
-local mvec3_set = mvector3.set
-local mvec3_z = mvector3.z
-local mvec3_set_z = mvector3.set_z
-local mvec3_sub = mvector3.subtract
-local mvec3_norm = mvector3.normalize
-local mvec3_add = mvector3.add
-local mvec3_mul = mvector3.multiply
-local mvec3_lerp = mvector3.lerp
 local mvec3_cpy = mvector3.copy
-local mvec3_set_l = mvector3.set_length
-local mvec3_dot = mvector3.dot
-local mvec3_cross = mvector3.cross
-local mvec3_dis = mvector3.distance
-local mvec3_dis_sq = mvector3.distance_sq
-local mvec3_len = mvector3.length
-local mvec3_rot = mvector3.rotate_with
-local mvec3_set_static = mvector3.set_static
-local mrot_lookat = mrotation.set_look_at
-local mrot_slerp = mrotation.slerp
-local mrot_yaw = mrotation.yaw
-local math_abs = math.abs
-local math_max = math.max
-local math_min = math.min
 
-local tmp_vec1 = Vector3(0, 0, 0)
-local tmp_vec2 = Vector3(0, 0, 0)
-local tmp_vec3 = Vector3(0, 0, 0)
-local tmp_vec4 = Vector3(0, 0, 0)
+local math_ceil = math.ceil
+local math_abs = math.abs
 
 local react_shoot = AIAttentionObject.REACT_SHOOT
 local react_surprised = AIAttentionObject.REACT_SURPRISED
@@ -118,8 +94,8 @@ function CopActionWalk:_init()
 		elseif not managers.groupai:state():enemy_weapons_hot() then
 			self._simplified_path = nav_path
 		else
-			local good_pos = mvector3.copy(common_data.pos)
-			self._simplified_path = self._calculate_simplified_path(good_pos, nav_path, (not self._sync or self._common_data.stance.name == "ntl") and 2 or 1, self._sync, true)
+			local good_pos = mvec3_cpy(common_data.pos)
+			self._simplified_path = self._calculate_simplified_path(good_pos, nav_path, self._common_data.stance.name == "ntl" and 2 or 1, true, true)
 		end
 	else
 		local nav_path = self._nav_path
@@ -178,8 +154,8 @@ function CopActionWalk:_init()
 		elseif not managers.groupai:state():enemy_weapons_hot() then
 			self._simplified_path = nav_path
 		else
-			local good_pos = mvector3.copy(common_data.pos)
-			self._simplified_path = self._calculate_simplified_path(good_pos, nav_path, (not self._sync or self._common_data.stance.name == "ntl") and 2 or 1, self._sync, true)
+			local good_pos = mvec3_cpy(common_data.pos)
+			self._simplified_path = self._calculate_simplified_path(good_pos, nav_path, 2, false, true)
 		end
 	end
 
@@ -218,7 +194,7 @@ function CopActionWalk:_init()
 				yaw = 360 + yaw
 			end
 
-			sync_yaw = 1 + math.ceil(yaw * 254 / 360)
+			sync_yaw = 1 + math_ceil(yaw * 254 / 360)
 		end
 
 		local sync_haste = self._haste == "walk" and 1 or 2
@@ -237,7 +213,7 @@ function CopActionWalk:_init()
 				nav_link_act_yaw = 360 + nav_link_act_yaw
 			end
 
-			nav_link_act_yaw = math.ceil(255 * nav_link_act_yaw / 360)
+			nav_link_act_yaw = math_ceil(255 * nav_link_act_yaw / 360)
 
 			if nav_link_act_yaw == 0 then
 				nav_link_act_yaw = 255
@@ -289,7 +265,7 @@ function CopActionWalk:_init()
 		self._unit:brain():rem_pos_rsrv("stand")
 		self._unit:brain():add_pos_rsrv("move_dest", {
 			radius = 30,
-			position = mvector3.copy(self._simplified_path[#self._simplified_path])
+			position = mvec3_cpy(self._simplified_path[#self._simplified_path])
 		})
 	end
 
